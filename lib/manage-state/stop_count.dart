91f76efdc1a8a57ca765867a076cfba558983d9f
devetechnologies <devetechnologies@gmail.com>
2024-02-07 07:55:31 -0400

    create buttons start and stop counter

--- lib/manage-state/stop_count.dart
+++ lib/manage-state/stop_count.dart
@@ -12,12 +12,34 @@ class StopCounterState extends StatefulWidget {
 class _StopCounterStateState extends State<StopCounterState> {
   int seconds = 0;
   Timer? timer;
+  bool isClick = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     timer = Timer.periodic(const Duration(seconds: 1), _runing);
+  }
+
+  void _runing(Timer timer) {
+    setState(() {
+      ++seconds;
+    });
+  }
+
+  void _stopCount() {
+    timer!.cancel();
+    setState(() {
+      isClick = false;
+    });
+  }
+
+  void _restartCount() {
+    timer = Timer.periodic(const Duration(seconds: 1), _runing);
+    setState(() {
+      seconds = 0;
+      isClick = true;
+    });
   }
 
   @override
@@ -27,18 +49,48 @@ class _StopCounterStateState extends State<StopCounterState> {
         centerTitle: true,
         title: const Text('State Manage'),
       ),
-      body: Center(
-        child: Text(
-          'seconds: $seconds',
-          style: Theme.of(context).textTheme.headlineMedium,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'seconds: $seconds',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: isClick ? _stopCount : null,
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white)),
+                  child: const Text('Stop Count'),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                TextButton(
+                  onPressed: isClick ? null : _restartCount,
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blueAccent),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white)),
+                  child: const Text('Restart Count'),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
-  }
-
-  void _runing(Timer timer) {
-    setState(() {
-      ++seconds;
-    });
   }
 }
