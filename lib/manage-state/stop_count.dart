8ee0977bca576c47a5c68d034e2a9772e906a2ea
devetechnologies <devetechnologies@gmail.com>
2024-02-09 08:08:03 -0400

    Working with Scroll

--- lib/manage-state/stop_count.dart
+++ lib/manage-state/stop_count.dart
@@ -13,17 +13,19 @@ class _StopCounterStateState extends State<StopCounterState> {
   int seconds = 0;
   Timer? timer;
   bool isClick = true;
+  int milliseconds = 0;
+  final listSeconds = <int>[];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    timer = Timer.periodic(const Duration(seconds: 1), _runing);
+    timer = Timer.periodic(const Duration(microseconds: 100), _runing);
   }
 
   void _runing(Timer timer) {
     setState(() {
-      ++seconds;
+      milliseconds += 100;
     });
   }
 
@@ -35,11 +37,17 @@ class _StopCounterStateState extends State<StopCounterState> {
   }
 
   void _restartCount() {
-    timer = Timer.periodic(const Duration(seconds: 1), _runing);
+    timer = Timer.periodic(const Duration(microseconds: 100), _runing);
+    listSeconds.clear();
     setState(() {
-      seconds = 0;
+      milliseconds = 0;
       isClick = true;
     });
+  }
+
+  String _secondText(int microseconds) {
+    final secondsText = milliseconds / 100;
+    return '$secondsText seconds';
   }
 
   @override
@@ -49,48 +57,86 @@ class _StopCounterStateState extends State<StopCounterState> {
         centerTitle: true,
         title: const Text('State Manage'),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'seconds: $seconds',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: isClick ? _stopCount : null,
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.green),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white)),
-                  child: const Text('Stop Count'),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                TextButton(
-                  onPressed: isClick ? null : _restartCount,
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blueAccent),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white)),
-                  child: const Text('Restart Count'),
-                )
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(child: _buildSecondsContainer(context)),
+          Expanded(child: _buildCountList())
+        ],
       ),
+    );
+  }
+
+  void addValueListSeconds() {
+    setState(() {
+      listSeconds.add(milliseconds);
+      milliseconds = 0;
+    });
+  }
+
+  Widget _buildSecondsContainer(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _secondText(milliseconds),
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: isClick ? _stopCount : null,
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white)),
+                child: const Text('Stop Count'),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                onPressed: isClick ? addValueListSeconds : null,
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.yellow),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white)),
+                child: const Text('Add To List'),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              TextButton(
+                onPressed: isClick ? null : _restartCount,
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.redAccent),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white)),
+                child: const Text('Restart Count'),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCountList() {
+    return ListView(
+      children: [
+        for (int microseconds in listSeconds)
+          ListTile(
+            title: Text(_secondText(microseconds)),
+          )
+      ],
     );
   }
 }
